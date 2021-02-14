72e4fbc89d7a9ea92caf5eed825965b8488dac26
yitzy299 <hugocarter@hotmail.co.uk>
2021-02-14 12:17:01 +0000

    Proper join checking go brrr
    - Replaced the existing maintenance mode mixin with one that denies a player from joining instead of instantly kicking. This improves server performance and makes the denied joining process quick and easy

--- src/main/java/tech/dttp/serversimplified/mixin/MixinPlayerManager.java
+++ src/main/java/tech/dttp/serversimplified/mixin/MixinPlayerManager.java
@@ -1,5 +1,10 @@
 package tech.dttp.serversimplified.mixin;
 
+import com.mojang.authlib.GameProfile;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tech.dttp.serversimplified.ServerSimplified;
 import tech.dttp.serversimplified.commands.MaintenanceModeCommand;
 import tech.dttp.serversimplified.commands.VanishCommand;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
@@ -14,21 +19,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.net.SocketAddress;
+
 @Mixin(PlayerManager.class)
-public class MixinPlayerManager {
+public abstract class MixinPlayerManager {
+
+    @Shadow public abstract boolean isOperator(GameProfile profile);
 
     @Inject(method = "onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At("RETURN"))
     public void onPlayerConnect(ClientConnection connection, ServerPlayerEntity entity, CallbackInfo info) {
         connection.send(new PlayerListS2CPacket(PlayerListS2CPacket.Action.REMOVE_PLAYER, VanishCommand.getVanished()));
     }
 
-    @Inject(method = "onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At("TAIL"))
-    protected void checkMaintenance (ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
-        if (MaintenanceModeCommand.isMaintenance() && !player.hasPermissionLevel(3)) {
-            player.networkHandler.disconnect(new LiteralText(MaintenanceModeCommand.getReason()));
-            LogManager.getLogger().info("Kicked "+player.getName().asString()+" for maintenance mode");
-            return;
+    @Inject(method = "checkCanJoin(Ljava/net/SocketAddress;Lcom/mojang/authlib/GameProfile;)Lnet/minecraft/text/Text;", at = @At("RETURN"), cancellable = true)
+    private void checkMaintenance (SocketAddress sa, GameProfile profile, CallbackInfoReturnable cir) {
+        if(MaintenanceModeCommand.isMaintenance() && !((PlayerManager)(Object)this).isOperator(profile) && !ServerSimplified.getConfiguration().getPermissions().hasPermission(profile.getId().toString(), "maintenance")) {
+            cir.setReturnValue(new TranslatableText("Server is closed for maintenance"));
         }
     }
-
 }
