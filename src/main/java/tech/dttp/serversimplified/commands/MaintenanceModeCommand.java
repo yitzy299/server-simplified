828c05c297da541aa5d50997cf8705e717626330
yitzy299 <hugocarter@hotmail.co.uk>
2021-02-15 17:51:57 +0000

    Maintenance mode improvements
    - Maintenance mode command now has on/off options, rather than a toggle
    - Automatically kicks players when maintenance mode is enabled

--- src/main/java/tech/dttp/serversimplified/commands/MaintenanceModeCommand.java
+++ src/main/java/tech/dttp/serversimplified/commands/MaintenanceModeCommand.java
@@ -2,7 +2,9 @@ package tech.dttp.serversimplified.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 import tech.dttp.serversimplified.ServerSimplified;
@@ -12,13 +14,14 @@ import static net.minecraft.server.command.CommandManager.literal;
 public class MaintenanceModeCommand {
     private static boolean maintenance = false;
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(literal("maintenance").requires(scs -> scs.hasPermissionLevel(3) || ServerSimplified.getConfiguration().getPermissions().checkPermissions(scs, "maintenance")).executes(context -> toggle(context.getSource())));
-    }
-
-    private static int toggle(ServerCommandSource scs) {
-        maintenance = !maintenance;
-        scs.sendFeedback(new LiteralText("Maintenance mode ").formatted(Formatting.ITALIC).formatted(Formatting.GRAY).append(new LiteralText (maintenance ? "on" : "off").formatted(maintenance ? Formatting.RED : Formatting.GREEN)), true);
-        return 1;
+        dispatcher.register(literal("maintenance").requires(scs -> {
+            try {
+                return scs.hasPermissionLevel(3) || ServerSimplified.getConfiguration().getPermissions().hasPermission(scs.getPlayer().getUuidAsString(), "maintenance");
+            } catch (CommandSyntaxException e) {
+                return false;
+            }
+        }).then(literal("on").executes(ctx -> setMaintenanceMode(ctx.getSource(), true))).then(literal("off").executes(ctx -> setMaintenanceMode(ctx.getSource(), false))));
+        //dispatcher.register(literal("maintenance").requires(scs -> scs.hasPermissionLevel(3) || ServerSimplified.getConfiguration().getPermissions().checkPermissions(scs, "maintenance")).executes(context -> toggle(context.getSource())));
     }
 
     public static boolean isMaintenance() {
@@ -27,6 +30,19 @@ public class MaintenanceModeCommand {
 
     public static String getReason() {
         return "Server is closed for maintenance";
+    }
+
+    private static int setMaintenanceMode(ServerCommandSource scs, boolean value) {
+        maintenance = value;
+        scs.sendFeedback(new LiteralText("Maintenance mode ").formatted(Formatting.ITALIC).formatted(Formatting.GRAY).append(new LiteralText (maintenance ? "on" : "off").formatted(maintenance ? Formatting.RED : Formatting.GREEN)), true);
+        if(maintenance) {
+            scs.getMinecraftServer().getPlayerManager().getPlayerList().forEach(player -> {
+                if(!player.hasPermissionLevel(3) && !ServerSimplified.getConfiguration().getPermissions().hasPermission(player.getUuidAsString(), "maintenance")) {
+                    player.networkHandler.disconnect(new LiteralText("Server has closed for maintenance"));
+                }
+            });
+        }
+        return 1;
     }
     public static void setMaintenanceMode(boolean value) {
         maintenance = value;
